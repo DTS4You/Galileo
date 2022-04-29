a2f14ffd12784fb908f87014a55d4a4b3af9ea19
Norbert.Schwarz <info@datentechnikschwarz.de>
2022-04-29 19:05:16 +0200

    Init

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from machine import Pin, Timer                              # RaspberryPi Pico2040 -> Hardware-Library
 from module_init import Global_Module as MyModule
 #import module_serial
-#import time
+import time
 
 # led = Pin(25, Pin.OUT)        # Debug LED
 flag = False
@@ -51,33 +51,17 @@ def do_loop():
 # ------------------------------------------------------------------------------
 def main():
 
-    if MyModule.inc_ws2812:
-        print("WS2812 -> Load-Module")
-        import module_ws2812_v2 as MyWS2812         # Modul WS2812  -> WS2812-Ansteuerung
-        print("WS2812 -> Setup")
-        MyWS2812.setup_ws2812()
-        print("WS2812 -> Run self test")
-        MyWS2812.self_test()
-        print("WS2812 -> Blink Test")
-        MyWS2812.do_blink_test()
-        print("WS2812 -> Dot-Test")
-        MyWS2812.do_dot_test()
-
-    if MyModule.inc_decoder:
-        print("Decode -> Load-Module")
-        import module_decode as MyDecode
-        print("Decode -> Setup")
-        MyDecode.decode_setup()
-        print("Decode -> Test")
-        MyDecode.decode_test("Test")
-
-    if MyModule.inc_serial:
-        print("Serial-COM -> Load-Module")
-        import module_serial as MySerial
-        print("Serial-Con -> Setup")
-        MySerial.sercon_setup()
-        print("Serial-Con -> Test")
-        MySerial.sercon_write_out("Test")
+    print("=== Start Main ===")
+    
+       
+    while MySerial.sercon_read_flag():
+        MySerial.sercon_read_line()
+            
+        
+        # Loop-Delay !!!
+        time.sleep(0.01)
+        
+    MySerial.sercon_write_out("End of Loop")
 
 
     print("End of Main !")
@@ -91,7 +75,39 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    if MyModule.inc_ws2812:
+        print("WS2812 -> Load-Module")
+        import module_ws2812_v2 as MyWS2812         # Modul WS2812  -> WS2812-Ansteuerung
+        print("WS2812 -> Setup")
+        MyWS2812.setup_ws2812()
+        ### Test ###
+        print("WS2812 -> Run self test")
+        MyWS2812.self_test()
+        print("WS2812 -> Blink Test")
+        MyWS2812.do_blink_test()
+        print("WS2812 -> Dot-Test")
+        MyWS2812.do_dot_test()
+
+    if MyModule.inc_decoder:
+        print("Decode -> Load-Module")
+        import module_decode as MyDecode
+        print("Decode -> Setup")
+        MyDecode.decode_setup()
+        ### Test ###
+        print("Decode -> Test")
+        MyDecode.decode_test("Test")
+
+    if MyModule.inc_serial:
+        print("Serial-COM -> Load-Module")
+        import module_serial as MySerial
+        print("Serial-Con -> Setup")
+        MySerial.sercon_setup()
+        ### Test ###
+        print("Serial-Con -> Test")
+        MySerial.sercon_write_out("Test")
+
+    main()      # Start Main $$$
 
 # Normal sollte das Programm hier nie ankommen !
 print("___End of Programm___ !!!")
